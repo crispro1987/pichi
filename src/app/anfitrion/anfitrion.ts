e5a37536d3264cad20965e6dcffc47156958a20e
crispro1987 <cristian@dosiscl.com>
2026-05-24 20:51:59 -0400

    cambios a angular 17

--- src/app/anfitrion/anfitrion.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-anfitrion',
-  imports: [],
-  templateUrl: './anfitrion.html',
-  styleUrl: './anfitrion.css',
-})
-export class Anfitrion {}
--- src/app/front/front.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-front',
-  imports: [],
-  templateUrl: './front.html',
-  styleUrl: './front.css',
-})
-export class Front {}
