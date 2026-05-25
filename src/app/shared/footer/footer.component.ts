68807dd81bc7f6474e130188c5db4b423a0bebc0
crispro1987 <cristian@dosiscl.com>
2026-05-24 21:37:39 -0400

    ahora angulae 17

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class FooterComponent {
+
+}
