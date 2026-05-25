4eaaf4c8c135362cdcd50a41b1dc676cbfd7686d
crispro1987 <cristian@dosiscl.com>
2026-05-24 20:43:27 -0400

    cambios por problemas

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,9 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   imports: [],
-  templateUrl: './footer.html',
-  encapsulation: ViewEncapsulation.None
+  templateUrl: './footer.html'
 })
 export class Footer {}
