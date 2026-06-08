1d0f75e09d25de1d399ea9f8fa40b0a11de6ee1b
crispro1987 <cristian@dosiscl.com>
2026-06-07 21:44:10 -0400

    Avance en front

--- src/app/front/front.routes.ts
+++ src/app/front/front.routes.ts
@@ -4,13 +4,15 @@ import { HospedajeComponent } from "./hospedaje/hospedaje.component";
 import { EntornoComponent } from "./entorno/entorno.component";
 import { EventosComponent } from "./eventos/eventos.component";
 import { LegalComponent } from "./legal/legal.component";
+import { HospedajesComponent } from "./hospedajes/hospedajes.component";
 
 export const FRONT_ROUTES: Routes = [
     { 
         path: '', children: [
             { path: 'inicio', component: FrontComponent },
             { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-            { path: 'hospedaje', component: HospedajeComponent },
+            { path: 'hospedaje/:id', component: HospedajeComponent },
+            { path: 'hospedajes', component: HospedajesComponent },
             { path: 'entorno', component: EntornoComponent },
             { path: 'eventos', component: EventosComponent },
             { path: 'legal', component: LegalComponent },
---
+++ src/app/front/hospedajes/hospedajes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hospedajes',
+  standalone: true,
+  imports: [],
+  templateUrl: './hospedajes.component.html',
+  styleUrl: './hospedajes.component.css'
+})
+export class HospedajesComponent {
+
+}
